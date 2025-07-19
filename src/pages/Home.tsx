a0681e8fababe0358694c4d162289a2d8187606d
nutcas3 <maurice.nyanja.e@gmail.com>
2025-07-20 01:28:46 +0300

    const [tags] = useState([     "NFT",     "Metaverse",     "Sustainable",     "Sonder",     "FOMO",     "Ghosting",   ]);

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
 import { Header } from "@/components/features/Header";
 import { HeroSearch } from "@/components/features/HeroSearch";
 import { TagList } from "@/components/features/TagList";
 
+// Static data moved outside component to prevent recreation on each render
+const TRENDING_TAGS = [
+  "NFT",
+  "Metaverse",
+  "Sustainable",
+  "Sonder",
+  "FOMO",
+  "Ghosting",
+];
+
 export function Home() {
-  const [tags] = useState([
-    "NFT",
-    "Metaverse",
-    "Sustainable",
-    "Sonder",
-    "FOMO",
-    "Ghosting",
-  ]);
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-black to-gray-900">
       <Header />
       <main className="pb-12">
         <HeroSearch />
-        <TagList title="Trending" tags={tags} />
-        <TagList title="For you" tags={tags} />
+        <TagList title="Trending" tags={TRENDING_TAGS} />
+        <TagList title="For you" tags={TRENDING_TAGS} />
       </main>
     </div>
   );
